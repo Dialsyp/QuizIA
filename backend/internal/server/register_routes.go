bba9e7d04a82a1b70688e58273048a1907df9cb0
dialsyp <dialsyphax@gmail.com>
2026-02-26 21:04:23 +0100

    changing the allowed origins for CORS to the production URL and redirecting to the frontend URL after authentication and logout.

--- backend/internal/server/register_routes.go
+++ backend/internal/server/register_routes.go
@@ -6,14 +6,13 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{os.Getenv("VITE_URL_FRONTEND")},
+		AllowOrigins:     []string{"https://quiz-ia-pi.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type"},
 		AllowCredentials: true,
